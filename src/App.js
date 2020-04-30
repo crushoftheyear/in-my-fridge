08ecbd28ab01e76bba33897019bcd17ff14e6122
crushoftheyear <kajsalovisaholmstrom@gmail.com>
2020-04-30 22:40:02 +0200

    Localstorage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { createStore, combineReducers } from '@reduxjs/toolkit'
 import styled from 'styled-components/macro'
 import 'normalize.css'
 
@@ -17,7 +17,25 @@ const reducer = combineReducers({
   fridge: fridge.reducer
 })
 
-const store = configureStore({ reducer })
+// Retrieve the localstorage & use as initial state
+const persistedStateJSON = localStorage.getItem('todosReduxState')
+let persistedState = {}
+
+if (persistedStateJSON) {
+  persistedState = JSON.parse(persistedStateJSON)
+}
+
+// Create store using the initial state
+const store = createStore(
+  reducer,
+  persistedState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+// Store the state in localstorage on any Redux state change
+store.subscribe(() => {
+  localStorage.setItem('todosReduxState', JSON.stringify(store.getState()))
+})
 
 export const App = () => {
   return (
